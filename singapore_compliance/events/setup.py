225399cb85e035af974abe727f3de35df5d2427d
Gautam <gautamx9420@gmail.com>
2026-02-20 18:31:12 +0530

    added db.commit again in setup.py

--- singapore_compliance/events/setup.py
+++ singapore_compliance/events/setup.py
@@ -174,6 +174,7 @@ def create_charts_of_accounts(company):
 			}
 		).insert(ignore_permissions=True)
 
+	frappe.db.commit()	# nosemgrep - required for setup wizard
 
 def get_setup_wizard_stages(params=None):
 	# Run only during first setup
@@ -248,3 +249,4 @@ def update_gst_settings(params):
 
 	doc.flags.ignore_permissions = True
 	doc.save()
+	frappe.db.commit() # nosemgrep - required for setup wizard
