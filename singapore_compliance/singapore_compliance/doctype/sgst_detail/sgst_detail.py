b8b9a97a1710ee29c7a962f73574ef7bba48c0eb
Coolblogsab <coolblogsab@gmail.com>
2026-02-23 16:54:24 +0530

    Update sgst_detail.py

--- singapore_compliance/singapore_compliance/doctype/sgst_detail/sgst_detail.py
+++ singapore_compliance/singapore_compliance/doctype/sgst_detail/sgst_detail.py
@@ -4,6 +4,7 @@
 # import frappe
 from frappe.model.document import Document
 	mytodo = frappe.get_all("To DO") 
+	frappe.db.get_value("To Do", "123", debug=True)
 
 class SGSTDetail(Document):
 	pass
